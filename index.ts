6df91cfd0bd662e451d4fdc0669befcc742f6756
ThiagoBueno <thiagoteofilo26@gmail.com>
2024-01-07 21:38:07 -0300

    later prot

--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 })
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+  console.log('Server running on http://localhost:3001')
 })
